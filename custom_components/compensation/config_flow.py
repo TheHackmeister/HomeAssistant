ce8bb11051e3edbf81dcb6134ccd092cbbf4493b
Spencer <tms@spencerslab.com>
2021-02-23 14:54:54 -0700

    Clean up config_flow output and improve validation and error handling.

--- custom_components/compensation/config_flow.py
+++ custom_components/compensation/config_flow.py
@@ -58,7 +58,8 @@ class CompensationConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             data_schema=vol.Schema(
                 {
                     vol.Required(CONF_TRACKED_ENTITY_ID): vol.In(
-                        { ent.entity_id: f"{ ent.name } ({ ent.entity_id })" for ent in self.hass.states.async_all(DOMAIN_SENSOR) }
+                        # Note: I considered filtering this to only contain states that were floats, but realized that would exclude some valid attributes we could track on some states. 
+                        { ent.entity_id: f"{ ent.name } ({ ent.entity_id }) - { ent.state }" for ent in self.hass.states.async_all(DOMAIN_SENSOR) }
                     ),
                 }
             ),
@@ -67,13 +68,15 @@ class CompensationConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_configure(self, user_input=None):
         """Step when user configures a calibrated sensor."""
-        self._errors = {}
+        errors = {}
 
         if user_input.get(CONF_ENTITY_ID):
             await self.async_set_unique_id( user_input.get(CONF_ENTITY_ID) )
             self._abort_if_unique_id_configured()
 
-            _LOGGER.warning("Made it in user_input: %s", user_input)
+            try:
+                # Validate the user provided entity_id.
+                cv.entity_id(user_input[CONF_ENTITY_ID])
             return self.async_create_entry(
                 title=f"{ self.unique_id }",
                 data={
@@ -86,23 +89,26 @@ class CompensationConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                     CONF_UNIT_OF_MEASUREMENT: user_input[CONF_UNIT_OF_MEASUREMENT],
                 },
             )
+            except vol.Invalid as e:
+                errors[CONF_ENTITY_ID] = "entity_id_not_valid"
         
+        tracked_entity_id = user_input.get(CONF_TRACKED_ENTITY_ID, self.entity_id_from_user_step)
         return self.async_show_form(
             step_id="configure",
             data_schema=vol.Schema(
                 {
-                    vol.Required(CONF_ENTITY_ID, default=f"{ user_input[CONF_TRACKED_ENTITY_ID] }_calibrated"): cv.string,
-                    vol.Optional(CONF_NAME, default=f"{ self.hass.states.get(user_input[CONF_TRACKED_ENTITY_ID]).name } Calibrated"): cv.string,
-                    vol.Optional(CONF_ATTRIBUTE): vol.In( list(self.hass.states.get(user_input[CONF_TRACKED_ENTITY_ID]).attributes.keys()) ),
+                    vol.Required(CONF_ENTITY_ID, default=f"{ tracked_entity_id }_calibrated"): cv.string,
+                    vol.Optional(CONF_NAME, default=f"{ self.hass.states.get(tracked_entity_id).name } Calibrated"): cv.string,
+                    vol.Optional(CONF_ATTRIBUTE): vol.In( list(self.hass.states.get(tracked_entity_id).attributes.keys()) ),
                     vol.Optional(CONF_PRECISION, default=DEFAULT_PRECISION): cv.positive_int,
                     vol.Optional(CONF_DEGREE, default=DEFAULT_DEGREE): vol.All(
                         vol.Coerce(int),
                         vol.Range(min=1, max=7),
                     ),
-                    vol.Optional(CONF_UNIT_OF_MEASUREMENT, default=f"{ self.hass.states.get(user_input[CONF_TRACKED_ENTITY_ID]).attributes.get(CONF_UNIT_OF_MEASUREMENT) }"): cv.string,
+                    vol.Optional(CONF_UNIT_OF_MEASUREMENT, default=f"{ self.hass.states.get(tracked_entity_id).attributes.get(CONF_UNIT_OF_MEASUREMENT) }"): cv.string,
                 }
             ),
-            errors=self._errors,
+            errors=errors,
         )
 
     async def async_step_import(self, user_input=None):
@@ -189,15 +195,18 @@ class OptionsFlowHandler(config_entries.OptionsFlow):
                 self.hass.async_create_task(
                     options_update_listener(self.hass, entry)
                 )
-                except:
+                except (ValueError, TypeError, AttributeError) as e:
                     sensor_issues.append(entity_id)
                     self._errors = { "base": "state_not_a_float" }
 
-
             # Unset the datapoint. 
             user_input.pop(CONF_DATAPOINTS)
+
             if sensor_issues:
-                sensor_issues = f"\nProblematic Sensors: { ', '.join(sensor_issues) }"
+            sensor_issues = "\n- ".join(sensor_issues)
+            sensor_issues = f"\nProblematic Sensors: \n- { sensor_issues }"
+        else:
+            sensor_issues = ""
 
         return self.async_show_form(
             step_id="add_calibration_datapoints",
@@ -207,5 +216,5 @@ class OptionsFlowHandler(config_entries.OptionsFlow):
                 }
             ),
             errors=self._errors,
-            description_placeholders={CONF_TRACKED_ENTITY_ID: sensor_issues}
+            description_placeholders={CONF_ENTITY_ID: "\n- " + "\n- ".join([f"{ entity_id }:\n { self._config_entries[entity_id].options.get(CONF_DATAPOINTS) }\n" for entity_id in self._calibrating_entities]), CONF_TRACKED_ENTITY_ID: sensor_issues}
         )
